197d78745777d6e82081a297a41ed3726b448755
Kabir Kwatra <kabir@kwatra.me>
2019-06-27 23:19:34 -0700

    Improved Database
    Database now uses hashing to hide the identity of any guild
    Added methods to allow storage of Authorized Role IDs

--- src/main/java/hackweek/group/filterbot/Database.kt
+++ src/main/java/hackweek/group/filterbot/Database.kt
@@ -6,13 +6,17 @@ import com.google.cloud.firestore.DocumentReference
 import com.google.cloud.firestore.Firestore
 import com.google.cloud.firestore.FirestoreOptions
 import com.google.cloud.firestore.WriteResult
+import com.google.common.hash.Hashing.sha256
+import java.nio.charset.Charset
 
 /**
- * Firestore based Database
+ * Firestore based Database to store specific data
  *
  * @constructor Creates a Firestore Database given credentials and a projectID
+ * @param gcpAuth Google Cloud Credentials for message scanning
+ * @param gcpProjectID Project ID for Google Cloud Project
  * */
-class Database(gcpAuth: GoogleCredentials, gcpProjectId: String) {
+class Database(gcpAuth: GoogleCredentials, gcpProjectID: String) {
     companion object {
         const val DEFAULT_COMMAND_PREFIX = "f!"
     }
@@ -20,7 +24,7 @@ class Database(gcpAuth: GoogleCredentials, gcpProjectId: String) {
     // Firestore Database
     private val db: Firestore = FirestoreOptions.getDefaultInstance()
         .toBuilder()
-        .setProjectId(gcpProjectId)
+        .setProjectId(gcpProjectID)
         .setCredentials(gcpAuth)
         .build()
         .service
@@ -30,7 +34,8 @@ class Database(gcpAuth: GoogleCredentials, gcpProjectId: String) {
      *  @param guildID ID of a Guild
      * */
     fun getCommandPrefix(guildID: String): String {
-        val docRef: DocumentReference = db.collection("guilds").document(guildID)
+        val guildIDHash = guildID.hashed()
+        val docRef: DocumentReference = db.collection("guilds").document(guildIDHash)
         val data: MutableMap<String, Any> = docRef.get().get().data ?: return DEFAULT_COMMAND_PREFIX
         return data["commandPrefix"] as String? ?: DEFAULT_COMMAND_PREFIX
         // default command prefix if firestore returns null
@@ -42,7 +47,8 @@ class Database(gcpAuth: GoogleCredentials, gcpProjectId: String) {
      * @param prefix Command Prefix
      */
     fun setCommandPrefix(guildID: String, prefix: String) {
-        val docRef: DocumentReference = db.collection("guilds").document(guildID)
+        val guildIDHash = guildID.hashed()
+        val docRef: DocumentReference = db.collection("guilds").document(guildIDHash)
         val data: MutableMap<String, Any> = docRef.get().get().data!!
         data["commandPrefix"] = prefix
         val result: ApiFuture<WriteResult> = docRef.set(data)
@@ -55,7 +61,8 @@ class Database(gcpAuth: GoogleCredentials, gcpProjectId: String) {
      */
     @Suppress("UNCHECKED_CAST")
     fun getFilters(guildID: String): List<String> {
-        val docRef: DocumentReference = db.collection("guilds").document(guildID)
+        val guildIDHash = guildID.hashed()
+        val docRef: DocumentReference = db.collection("guilds").document(guildIDHash)
         val data: MutableMap<String, Any> = docRef.get().get().data ?: mutableMapOf()
         return (data["filters"] ?: listOf<String>()) as List<String>
     }
@@ -66,7 +73,8 @@ class Database(gcpAuth: GoogleCredentials, gcpProjectId: String) {
      *  @param filters List of filters
      */
     fun setFilters(guildID: String, filters: MutableList<String>) {
-        val docRef: DocumentReference = db.collection("guilds").document(guildID)
+        val guildIDHash = guildID.hashed()
+        val docRef: DocumentReference = db.collection("guilds").document(guildIDHash)
         val data: MutableMap<String, Any> = docRef.get().get().data ?: mutableMapOf()
         data["filters"] = filters
         val result: ApiFuture<WriteResult> = docRef.set(data)
@@ -80,7 +88,8 @@ class Database(gcpAuth: GoogleCredentials, gcpProjectId: String) {
      */
     @Suppress("UNCHECKED_CAST")
     fun addFilters(guildID: String, filters: MutableList<String>) {
-        val docRef: DocumentReference = db.collection("guilds").document(guildID)
+        val guildIDHash = guildID.hashed()
+        val docRef: DocumentReference = db.collection("guilds").document(guildIDHash)
         val data: MutableMap<String, Any> =
             docRef.get().get().data ?: mutableMapOf<String, MutableList<String>>() as MutableMap<String, Any>
         if (data["filters"] == null) data["filters"] = mutableListOf<String>()
@@ -88,4 +97,72 @@ class Database(gcpAuth: GoogleCredentials, gcpProjectId: String) {
         val result: ApiFuture<WriteResult> = docRef.set(data)
         result.get()
     }
+
+    /**
+     * Adds role IDs that are considered authorized to database
+     * @param guildID ID of a Guild
+     * @param roleIds List of Authorized roleIDs
+     */
+    @Suppress("UNCHECKED_CAST")
+    fun addAuthorizedRoleIDs(guildID: String, roleIds: List<String>) {
+        val guildIDHash = guildID.hashed()
+        val roleIDsHashed = mutableListOf<String>()
+        roleIds.forEach { roleIDsHashed.add(it.hashed()) }
+        val docRef: DocumentReference = db.collection("guilds").document(guildIDHash)
+        val data: MutableMap<String, Any> =
+            docRef.get().get().data ?: mutableMapOf<String, MutableList<String>>() as MutableMap<String, Any>
+        if (data["authorizedRoleIDs"] == null) data["authorizedRoleIDs"] = mutableListOf<String>()
+        (data["authorizedRoleIDs"] as MutableList<String>).addAll(roleIDsHashed)
+        val result: ApiFuture<WriteResult> = docRef.set(data)
+        result.get()
+    }
+
+    /**
+     * De-authorizes role IDs and removes from database
+     * @param guildID ID of a Guild
+     * @param roleIds List of Unauthorized role IDs
+     * @return if any role ID was removed
+     */
+    @Suppress("UNCHECKED_CAST")
+    fun removeAuthorizedRoleIDs(guildID: String, roleIds: List<String>): Boolean {
+        val guildIDHash = guildID.hashed()
+        val roleIDsHashed = mutableListOf<String>()
+        roleIds.forEach { roleIDsHashed.add(it.hashed()) }
+        val docRef: DocumentReference = db.collection("guilds").document(guildIDHash)
+        val data: MutableMap<String, Any> =
+            docRef.get().get().data ?: mutableMapOf<String, MutableList<String>>() as MutableMap<String, Any>
+        if (data["authorizedRoleIDs"] == null) data["authorizedRoleIDs"] = mutableListOf<String>()
+        val authorizedRoleIDsHashed = data["authorizedRoleIDs"] as List<String>
+        val newAuthorizedRoleIDsHashed = authorizedRoleIDsHashed.filter { !roleIDsHashed.contains(it) }
+        return if (authorizedRoleIDsHashed == newAuthorizedRoleIDsHashed) false
+        else {
+            data["authorizedRoleIDs"] = newAuthorizedRoleIDsHashed
+            val result: ApiFuture<WriteResult> = docRef.set(data)
+            result.get()
+            true
+        }
+    }
+
+    /**
+     * Checks if a role ID is authorized
+     * @param guildID ID of a Guild
+     * @param roleID role ID to check
+     * @return if role ID is authorized
+     */
+    @Suppress("UNCHECKED_CAST")
+    fun isAuthorizedRoleID(guildID: String, roleID: String): Boolean {
+        val guildIDHash = guildID.hashed()
+        val roleIDHashed = roleID.hashed()
+        val docRef: DocumentReference = db.collection("guilds").document(guildIDHash)
+        val data: MutableMap<String, Any> =
+            docRef.get().get().data ?: mutableMapOf<String, MutableList<String>>() as MutableMap<String, Any>
+        if (data["authorizedRoleIDs"] == null) data["authorizedRoleIDs"] = mutableListOf<String>()
+        return (data["authorizedRoleIDs"] as MutableList<String>).contains(roleIDHashed)
+    }
+
+    /**
+     * Hashes current string
+     * @return Hashed String
+     */
+    private fun String.hashed() = sha256().hashString(this, Charset.defaultCharset()).toString()
 }
